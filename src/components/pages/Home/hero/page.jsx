db319661c899b5266c702b1fa438993c9aa40a84
Nishan9925 <zakaryannshan@gmail.com>
2025-08-23 22:42:37 +0400

    hero data fetch and styles

--- src/components/pages/Home/hero/page.jsx
+++ src/components/pages/Home/hero/page.jsx
@@ -1,31 +1,26 @@
 import Image from "next/image";
 
 function Hero({ imageSrc, imageAlt, data }) {
-  console.log("Hero Data:", data.fields); //the actual title 
+  const heroTitle = data?.fields?.title;
+
+  // console.log("Hero Data:", data.fields); //the actual title
   // console.log(Array.isArray(data), data.length);
   return (
-    <section id="hero" className="w-100% h-screen">
-      {Object.entries(data).map(([key, value]) => {
-        // console.log(value.title);
-        return (
-          <div key={key}>
-            <Image
-              src={imageSrc}
-              alt={imageAlt}
-              // width={0}
-            // height={0}
-            // sizes="100vw"
-            className="w-100% h-100% object-cover z-[-1] "
-            priority
-            fill
-          />
+    <section id="hero" className="w-full max-h-[800px]">
+      <div className=" relative w-full aspect-[21/9] max-h-[500px] min-h-[400px] overflow-hidden">
+        <Image
+          src={imageSrc}
+          alt={imageAlt}
+          className="object-cover"
+          priority
+          fill
+        />
+        <div className="absolute inset-0 bg-black/30"></div>
+      </div>
 
-          <div className="absolute inset-0 bg-black/30"></div>
-          <div className="relative z-0 inset-0 flex items-center justify-center bg-opacity-50">
-            <h1 className="text-red-900 text-4xl font-bold">{value.title}</h1>
-          </div>
-        </div>
-      )})}
+      <div className="absolute z-0 inset-0 flex items-center justify-start">
+        <h1 className="text-red-900 text-4xl font-bold">{heroTitle}</h1>
+      </div>
     </section>
   );
 }
