4b6d0f94d2341fb857fd0e09de2b5ee78485594c
Nishan9925 <zakaryannshan@gmail.com>
2025-08-26 01:11:05 +0400

    temp rooms page

---
+++ src/app/lib/About.js
@@ -0,0 +1,5 @@
+import BaseRepository from "./BaseRepository";
+
+export default class AboutRepository extends BaseRepository {
+    static contentType = "aboutUs";
+}
---
+++ src/app/rooms/page.jsx
@@ -0,0 +1,10 @@
+function Rooms() {
+    return (
+        <section>
+            <h2>Our Rooms</h2>
+            <p>Explore our range of comfortable and stylish rooms.</p>
+        </section>
+    )
+}
+
+export default Rooms;
